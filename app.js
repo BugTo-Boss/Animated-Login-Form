76c35323bdfc144c8a69434add6ab826ad0e89be
BugTo-Boss <mr.bugtoboss@gmail.com>
2025-10-15 17:32:49 +0330

    final

---
+++ app.js
@@ -0,0 +1,33 @@
+const toggleIcons = document.querySelectorAll('.toggle-pass');
+
+toggleIcons.forEach(icon => {
+    icon.addEventListener('click', () => {
+        const input = icon.previousElementSibling;
+        if (input.type === 'password') {
+            input.type = 'text';
+            icon.classList.remove('fa-eye');
+            icon.classList.add('fa-eye-slash');
+        } else {
+            input.type = 'password';
+            icon.classList.remove('fa-eye-slash');
+            icon.classList.add('fa-eye');
+        }
+    });
+});
+
+const loginForm = document.querySelector(".login-form");
+const signupForm = document.querySelector(".signup-form");
+const goSignUp = document.getElementById("goSignUp");
+const goLogin = document.getElementById("goLogin");
+
+goSignUp.addEventListener("click" , (e) =>{
+    e.preventDefault();
+    loginForm.classList.remove("active");
+    signupForm.classList.add("active");
+})
+
+goLogin.addEventListener("click" , (e) =>{
+    e.preventDefault();
+    signupForm.classList.remove("active");
+    loginForm.classList.add("active");
+})
